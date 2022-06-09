33a742344e5f07f295ae7054b628b1dfa8f52437
Shahaban Mallick <mallickshaban2@gmail.com>
2022-06-09 11:56:16 +0530

    Initial commit
    working on IndianElectricity new look and adding backend, solo project. working progress.

---
+++ script.js
@@ -0,0 +1,68 @@
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+// faq
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+
+// login
+
+let logBtn = document.querySelector('#log-btn');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+
+
+
+
+logBtn.addEventListener('click', () => {
+logBtn.classList.toggle('fa-times');
+
+});
+
+formBtn.addEventListener('click', () => {
+loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+loginForm.classList.remove('active');
+
+
+});
